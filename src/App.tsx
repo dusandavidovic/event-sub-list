f32929b0beb1cdfee27e6d803765faae54096ad3
Dusan <dusandavidovic@gmail.com>
2023-04-07 13:15:54 -0400

    created grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Grid, GridItem } from "@chakra-ui/react";
 import SheetList from "./components/SheetList";
 
 function App() {
-  return <SheetList />;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"header" "main"`,
+        lg: `"header header" "main"`,
+      }}
+      // gridTemplateRows={"50px 1fr 30px"}
+      //gridTemplateColumns={"250px 1fr"}
+      // h="200px"
+      // gap="1"
+      // color="blackAlpha.700"
+    >
+      <GridItem pl="2" bg="gray.200" area={"header"}>
+        Header
+      </GridItem>
+
+      <GridItem pl="2" bg="blue.200" area={"main"}>
+        <SheetList />
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
