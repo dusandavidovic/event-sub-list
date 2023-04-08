30baede3eed49fdfaeba5fbbccc49b17aee33a5c
Dusan <dusandavidovic@gmail.com>
2023-04-08 13:52:20 -0400

    added Apply & Remove filters button

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, HStack } from "@chakra-ui/react";
 import { IFilter } from "../interface/filter";
 import FilterLine from "./FilterLine";
 
@@ -7,7 +8,31 @@ interface IFilterProps {
 }
 
 const Filter: React.FC<IFilterProps> = ({ columns, onFilterChange }) => {
-  return <FilterLine columns={columns} onFilterChange={onFilterChange} />;
+  return (
+    <>
+      <Box boxSize="35%">
+        <HStack padding={"10px"}>
+          <Button
+            width="170px"
+            type="submit"
+            bg={"blue.500"}
+            onClick={() => onFilterChange([], "apply")}
+          >
+            Apply Filters
+          </Button>
+          <Button
+            width="170px"
+            type="submit"
+            bg={"blue.500"}
+            onClick={() => onFilterChange([], "remove")}
+          >
+            Remove Filters
+          </Button>
+        </HStack>
+      </Box>
+      <FilterLine columns={columns} onFilterChange={onFilterChange} />
+    </>
+  );
 };
 
 export default Filter;
